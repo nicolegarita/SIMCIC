0ff25f06a236725def2058607db0a3d30ee2ea84
Nicole Garita <nicolegaritaconstante@hotmail.com>
2018-06-29 16:24:56 -0600

    script cambiado

---
+++ SIMCIC/SIMCIC.DATA/Medicamento.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIMCIC.DATA
+{
+    public class Medicamento
+    {
+        public int IdMedicamento { get; set; }
+        public string NombreMedicamento { get; set; }
+        public int Cantidad { get; set; }
+        public int IdCategoria { get; set; }
+        public string Suministracion { get; set; }
+        public string FechaVencimiento { get; set; }
+       
+    }
+}
---
+++ SIMCIC/SIMCIC.DATA/Pacientes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIMCIC.DATA
+{
+    public class Pacientes
+    {
+        public int Cedula { get; set; }
+        public string NombreCompleto { get; set; }
+        public string FechaNacimiento { get; set; }
+        public int Edad { get; set; }
+        public string Sexo { get; set; }
+        public int Telefono { get; set; }
+        public string Correo { get; set; }
+        public string Ocupacion { get; set; }
+    }
+}
